404c729c0eda4d2f2fca3a1c697eab421f7d123e
Luiz Otávio <Luiz.fontes0@outlook.com>
2026-06-26 17:17:52 -0300

    poucas coisas

--- src/main/java/com/main/servicoFinal/controller/UserController.java
+++ src/main/java/com/main/servicoFinal/controller/UserController.java
@@ -6,14 +6,12 @@ package com.main.servicoFinal.controller;
 
 import com.main.servicoFinal.model.User;
 import com.main.servicoFinal.model.UserUpd;
-import com.main.servicoFinal.model.UserLogar;
 import com.main.servicoFinal.model.UserPerfil;
 import com.main.servicoFinal.model.UserRegistro;
 import com.main.servicoFinal.service.TokenService;
 import com.main.servicoFinal.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/main/servicoFinal/repository/UsuarioServicoRepository.java
+++ src/main/java/com/main/servicoFinal/repository/UsuarioServicoRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioServicoRepository extends JpaRepository<UsuarioServicoDto, Long>{
    
 List<UsuarioServicoDto> findByUsuarioId(Long usuarioId);
+
 }
